3a1de44a5669d543f62ec8c4a5626d349ac7301a
KevinMattpq <kevinmpena05@gmail.com>
2023-11-02 20:12:21 -0700

    Update personal 07

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,18 @@
+require('.dotenv').config()
 const express = require('express');
 const router = express.Router();
 const cars = require('./cars');
 const buyers = require('./buyers');
+
+router.use(auth(config));
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    baseURL: 'http://localhost:3000',
+    clientID: '{yourClientId}',
+    issuerBaseURL: 'https://{yourDomain}',
+    secret: 'LONG_RANDOM_STRING'
+  };
 
 router.use['/',require('./swagger')]
 router.use('/cars', cars);
